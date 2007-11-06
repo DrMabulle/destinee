c023b041d3194674cc57326b3f5bd95ba3199886
amoros.pierre <amoros.pierre@ccf8bb3c-303e-0410-b281-fd1e72325955>
2007-11-06 20:18:02 +0000

    Ajout des attaques

---
+++ destinee/src/destinee/data/AttaqueBerserk.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package destinee.data;
+
+/**
+ * @author AMOROS
+ *
+ */
+public class AttaqueBerserk extends AttaqueAbstract implements Attaque
+{
+
+	/**
+	 * @param aPerso
+	 */
+	public AttaqueBerserk(Perso aPerso)
+	{
+		super(aPerso);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see destinee.data.Attaque#getBonusAtt()
+	 */
+	public int getBonusAtt()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see destinee.data.Attaque#getBonusDeg()
+	 */
+	public int getBonusDeg()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see destinee.data.Attaque#getNbDesAtt()
+	 */
+	public int getNbDesAtt()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see destinee.data.Attaque#getNbDesDeg()
+	 */
+	public int getNbDesDeg()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
