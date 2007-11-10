76845445a88630caf2bd1eef1e27ecb5abac91a9
DrMabulle <DrMabulle@ccf8bb3c-303e-0410-b281-fd1e72325955>
2007-11-10 14:36:50 +0000

    Copied remotely

---
+++ trunk/Infra/src/logic/rules/LogicRulesDB.java
@@ -0,0 +1,17 @@
+package logic.rules;
+
+import java.util.List;
+
+/**
+ * @author Benoit Kessler
+ * 
+ */
+public interface LogicRulesDB 
+{
+	/**
+	 * 
+	 * @return une liste de règles Prolog, dépendant des besoins
+	 */
+	public List<String> getRulesList();
+
+}
---
+++ trunk/Infra/tests/destinee/core/utils/ConversionUtilTest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package destinee.core.utils;
+
+import java.math.BigDecimal;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Bubulle
+ *
+ */
+public class ConversionUtilTest extends TestCase
+{
+	public void testBigDecimalVersString()
+	{
+		String result = null;
+		BigDecimal decimal = null;
+
+		// Test grosse valeur
+		decimal = new BigDecimal(258963.35785);
+		result = ConversionUtil.bigDecimalVersString(decimal, 3);
+		assertEquals("258 963,358", result);
+
+		// Valeur proche de 0
+		decimal = new BigDecimal(0.00000357844867486);
+		result = ConversionUtil.bigDecimalVersString(decimal, 10);
+		assertEquals("0,0000035784", result);
+	}
+
+	public void testTest()
+	{
+		
+		
+	}
+	public void testTest2()
+	{
+		
+	}
+}
