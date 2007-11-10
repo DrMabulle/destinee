68ebc32b444a111e0c5b096a81d333c043642f83
DrMabulle <DrMabulle@ccf8bb3c-303e-0410-b281-fd1e72325955>
2007-11-10 14:29:25 +0000

    Initial import.

---
+++ Infra/tests/logic/gateways/DestineeToPrologTest.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package logic.gateways;
+
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Bubulle
+ *
+ */
+public class DestineeToPrologTest extends TestCase
+{
+	private DestineeToLogicGateway itsGate;
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		itsGate = DestineeToLogicGatewayImpl.getDefaultInstance();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception
+	{
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link logic.gateways.DestineeToLogicGatewayImpl#query(java.lang.String)}.
+	 */
+	@SuppressWarnings("unchecked")
+	public void testQuery()
+	{
+		itsGate.ajouterPerso("Koumi", 12, 5, 12, -6, 10, 0);
+		itsGate.ajouterAttaquePerso("Koumi", "normale");
+		itsGate.ajouterAttaquePerso("Koumi", "brutale");
+		List<Map<String, String>> retour = itsGate.query("peutAttaquer(Perso, []).");
+		assertNotNull(retour);
+		assertTrue(!retour.isEmpty());
+		Map binding = retour.get(0);
+		String perso = (String) binding.get("Perso");
+		assertEquals("'Koumi'", perso);
+	}
+
+	/**
+	 * Test method for {@link logic.gateways.DestineeToLogicGatewayImpl#ajouterPerso(java.lang.String, int, int, int, int, int, int)}.
+	 */
+	public void testAjouterPerso()
+	{
+		itsGate.ajouterPerso("Koumi", 12, 5, 12, -6, 10, 0);
+	}
+
+	/**
+	 * Test method for {@link logic.gateways.DestineeToLogicGatewayImpl#ajouterAttaquePerso(java.lang.String, java.lang.String)}.
+	 */
+	public void testAjouterAttaquePerso()
+	{
+		itsGate.ajouterAttaquePerso("Koumi", "normale");
+		itsGate.ajouterAttaquePerso("Koumi", "brutale");
+	}
+
+}
