abed5bdb68796de97c431a9531fc9acd63f58df9
amoros.pierre <amoros.pierre@ccf8bb3c-303e-0410-b281-fd1e72325955>
2007-11-16 18:00:56 +0000

    bon cette fois j'espere que ca marche : deuxieme essai du commit précedent

--- destinee/src/destinee/main/Main.java
+++ destinee/src/destinee/main/Main.java
@@ -3,6 +3,16 @@ package destinee.main;
 import java.math.BigDecimal;
 
 import destinee.core.utils.ConversionUtil;
+import destinee.data.Attaque;
+import destinee.data.AttaqueBerserk;
+import destinee.data.AttaqueBrutale;
+import destinee.data.AttaqueImparable;
+import destinee.data.AttaqueKamikaze;
+import destinee.data.AttaqueMagique;
+import destinee.data.AttaqueNormale;
+import destinee.data.AttaquePrecise;
+import destinee.data.AttaqueRapide;
+import destinee.data.Cible;
 import destinee.data.Perso;
 import destinee.probas.Proba;
 import destinee.probas.ResolutionAttaque;
@@ -32,41 +42,41 @@ public class Main
 
 		Perso noone = new Perso(12,10,8,5,5,-3);
 		
-		
+		Cible cible = new Cible(5,3,9);
 		
 		int nbDAtt = noone.getNombreDeDesAttaque();
 		int bonusAtt = noone.getBonusAttaque();
-		//int nbDAtt = 12;
-		//int bonusAtt = 10;
+		
+		int nbDDef = cible.getNombreDeDesDefense();
+		int bonusDef = cible.getBonusDefense();
 
-		int nbDDef = 11;
-		int bonusDef = 3;
-
+		Attaque attaqueUn = new AttaqueNormale(noone);
+		Attaque attaqueDeux = new AttaqueImparable(noone);
+//		Attaque attaqueTrois = new AttaqueImparable(noone);
+		
 		startTime = System.currentTimeMillis();
-		System.out.println("Début du traitement : résolution d'une attaque (" + nbDAtt + "D+" + bonusAtt + " contre " + nbDDef + "D+" + bonusDef
+		System.out.println("Début du traitement : résolution d'une attaque précise (" + nbDAtt + "D+" + bonusAtt + " contre " + nbDDef + "D+" + bonusDef
 				+ ", reussite critique ");
-		BigDecimal resultat3 = ResolutionAttaque.resoudreAttaque(nbDAtt, bonusAtt, nbDDef, bonusDef, ResolutionAttaque.RESOLUTION_COUP_CRITIQUE);
+		BigDecimal resultat3 = ResolutionAttaque.ResoudreAttaque(attaqueDeux, cible,0);
 		stopTime = System.currentTimeMillis();
 		long time3 = stopTime - startTime;
 
 		startTime = System.currentTimeMillis();
-		System.out.println("Début du traitement : résolution d'une attaque (" + nbDAtt + "D+" + bonusAtt + " contre " + nbDDef + "D+" + bonusDef
-				+ ", reussite ");
-		BigDecimal resultat4 = ResolutionAttaque.resoudreAttaque(nbDAtt, bonusAtt, nbDDef, bonusDef, ResolutionAttaque.RESOLUTION_COUP_SIMPLE);
+		System.out.println("Début du traitement : dégats moyens d'une attaque normale (" + nbDAtt + "D+" + bonusAtt + " contre " + nbDDef + "D+" + bonusDef);
+		double resultat4 = ResolutionAttaque.esperanceDeDegats(attaqueUn, cible);
 		stopTime = System.currentTimeMillis();
 		long time4 = stopTime - startTime;
 
 		startTime = System.currentTimeMillis();
 		System.out
-				.println("Début du traitement : résolution d'une attaque (" + nbDAtt + "D+" + bonusAtt + " contre " + nbDDef + "D+" + bonusDef + ", esquive ");
-		BigDecimal resultat5 = ResolutionAttaque.resoudreAttaque(nbDAtt, bonusAtt, nbDDef, bonusDef, ResolutionAttaque.RESOLUTION_ESQUIVE_SIMPLE);
+				.println("Début du traitement : dégats moyens attaque brutale(" + nbDAtt + "D+" + bonusAtt + " contre " + nbDDef + "D+" + bonusDef);
+		double resultat5 = ResolutionAttaque.esperanceDeDegats(attaqueDeux, cible,0);
 		stopTime = System.currentTimeMillis();
 		long time5 = stopTime - startTime;
 
 		startTime = System.currentTimeMillis();
-		System.out.println("Début du traitement : résolution d'une attaque (" + nbDAtt + "D+" + bonusAtt + " contre " + nbDDef + "D+" + bonusDef
-				+ ", esquive critique ");
-		BigDecimal resultat6 = ResolutionAttaque.resoudreAttaque(nbDAtt, bonusAtt, nbDDef, bonusDef, ResolutionAttaque.RESOLUTION_ESQUIVE_PARFAITE);
+		System.out.println("Début du traitement : dégats moyens d'une attaque imparable(" + nbDAtt + "D+" + bonusAtt + " contre " + nbDDef + "D+" + bonusDef);
+		double resultat6 = ResolutionAttaque.esperanceDeDegats(attaqueDeux, cible,1);
 		stopTime = System.currentTimeMillis();
 		long time6 = stopTime - startTime;
 
@@ -77,19 +87,21 @@ public class Main
 		System.out.println("Traitement 2 exécuté en " + ConversionUtil.longVersStringFormat(time2) + " millisecondes");
 		System.out.println("proba(168, 18) = " + resultat2);
 		System.out.println("Traitement 3 exécuté en " + ConversionUtil.longVersStringFormat(time3) + " millisecondes");
-		System.out.println("chances de toucher avec " + nbDAtt + "D+" + bonusAtt + " sur " + nbDDef + "D+" + bonusDef + " (reussite critique ) : " 
-				+ ConversionUtil.bigDecimalVersString(resultat3, 12));
+		System.out.println("chances de toucher avec " + nbDAtt + "D+" + bonusAtt + " sur " + nbDDef + "D+" + bonusDef + " (reussite critique )(attaque précise) : " 
+				+ resultat3);
 		System.out.println("Traitement 4 exécuté en " + ConversionUtil.longVersStringFormat(time4) + " millisecondes");
-		System.out.println("chances de toucher avec " + nbDAtt + "D+" + bonusAtt + " sur " + nbDDef + "D+" + bonusDef + " (reussite normale ) : " 
-				+ ConversionUtil.bigDecimalVersString(resultat4, 12));
+		System.out.println("esperance de dégats avec " + nbDAtt + "D+" + bonusAtt + " sur " + nbDDef + "D+" + bonusDef +" : " 
+				+ resultat4);
 		System.out.println("Traitement 5 exécuté en " + ConversionUtil.longVersStringFormat(time5) + " millisecondes");
-		System.out.println("chances de toucher avec avec " + nbDAtt + "D+" + bonusAtt + " sur " + nbDDef + "D+" + bonusDef + " (esquive ) : " 
-				+ ConversionUtil.bigDecimalVersString(resultat5, 12));
+		System.out.println("esperance de dégats en attaque precise avec " + nbDAtt + "D+" + bonusAtt + " sur " + nbDDef + "D+" + bonusDef + "  : " 
+				+resultat5);
 		System.out.println("Traitement 6 exécuté en " + ConversionUtil.longVersStringFormat(time6) + " millisecondes");
-		System.out.println("chances de toucher avec avec " + nbDAtt + "D+" + bonusAtt + " sur " + nbDDef + "D+" + bonusDef + " (esquive critique ) : "
-				+ ConversionUtil.bigDecimalVersString(resultat6, 12));
+		System.out.println("esperance de dégats en attaque precise avec " + nbDAtt + "D+" + bonusAtt + " sur " + nbDDef + "D+" + bonusDef + " (esquive critique ) : "
+				+ resultat6);
 		System.out.println("Traitement total exécuté en " + ConversionUtil.longVersStringFormat(globalStopTime - globaStartTime) + " millisecondes");
-		System.out.println(ConversionUtil.bigDecimalVersString(resultat3.add(resultat4.add(resultat5.add(resultat6))), 12));
+		System.out.println(attaqueUn.getNbDesDeg());
+		System.out.println(attaqueDeux.getNbDesDeg());
+		System.out.println(attaqueDeux.getNbDesDegatsCritique());
 	}
 
 }
